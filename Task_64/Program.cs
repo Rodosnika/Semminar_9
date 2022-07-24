ba0cf2b5e7f8a4f6be062ea9b51e2911fb15c216
Veronika <vvnika@mail.ru>
2022-07-24 20:36:13 +1100

    add "Задача_64"

---
+++ Task_64/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+// 
+Console.WriteLine("Задача 64!");
+Console.Write("Введите натуральное число N,\nчтобы вывести все натуральные числа от N до 1: ");
+string n = Console.ReadLine();
+
+var count = ExceptionHandling(n);
+
+if (count.exception && count.number >= 0)
+{
+    Numbers(count.number);
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.DarkRed;
+    Console.WriteLine($"{n} -> Некорректный ввод!");
+}
+
+void Numbers(int last)
+{
+    if (last == 0)
+    {
+        return;
+    }
+    Console.Write($"{last} ");
+    Numbers(last - 1);
+}
+
+(bool exception, int number) ExceptionHandling(string number)
+{
+    bool yesInt = int.TryParse(number, out int d);
+    if (!yesInt)
+    {
+        return (false, d);
+    }
+    return (true, d);
+}
